4148026b0dac801a83fa02b6ff03386a4ec6bd6f
Sean Smith <seansmith1020@gmail.com>
2014-07-10 09:26:59 -0600

    passes tests

--- lib/employee_filter.rb
+++ lib/employee_filter.rb
@@ -1,3 +1,23 @@
+require "Date"
+
 class EmployeeFilter
 
+  def initialize(input)
+    @employees = input
+  end
+
+  def started_before_2006
+    date = Date.new(2006, 1, 1)
+    output = []
+    @employees.map do |hash|
+      output << hash if hash[:start_date] < date
+    end
+    output
+  end
+
+  def all_with_start_date
+    output = @employees.map do |hash|
+      "#{hash[:first_name]} #{hash[:last_name]} (#{hash[:title]}) - #{hash[:start_date].month}/#{hash[:start_date].day}/#{hash[:start_date].year}"
+    end
+  end
 end
